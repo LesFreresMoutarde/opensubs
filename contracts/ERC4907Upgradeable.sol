ae59562d4864a23c9fdbe84fab3cafec5546ec02
Emile Calixte <calixte.emile@gmail.com>
2022-11-29 12:30:32 +0100

    Rename ERC4907Upgradeable initialize function

--- contracts/ERC4907Upgradeable.sol
+++ contracts/ERC4907Upgradeable.sol
@@ -15,7 +15,7 @@ contract ERC4907Upgradeable is Initializable, ERC721Upgradeable, IERC4907 {
 
     mapping(uint256 => UserInfo) internal _users;
 
-    function initialize(string calldata name_, string calldata symbol_) internal onlyInitializing {
+    function __ERC4907_init(string calldata name_, string calldata symbol_) internal onlyInitializing {
         ERC721Upgradeable.__ERC721_init(name_, symbol_);
     }
 
