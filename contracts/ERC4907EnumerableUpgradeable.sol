1ded839b52dd3ab29ab218bc07c7389637469717
alexisljn <alexis.lejeune@gmx.fr>
2022-11-29 12:24:13 +0100

    Create ERC4907EnumerableUpgradeable contract and its storage variables

---
+++ contracts/ERC4907EnumerableUpgradeable.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "./ERC4907Upgradeable.sol";
+
+contract ERC4907EnumerableUpgradeable is Initializable, ERC4907Upgradeable {
+
+    // Mapping from user to list of used (borrowed) token IDs
+    mapping(address => mapping(uint256 => uint256)) private _usedTokens;
+
+    // Mapping from token ID to index of the user tokens list
+    mapping(uint256 => uint256) private _usedTokensIndex;
+
+    // Array with all used token ids, used for enumeration
+    uint256[] private _allUsedTokens;
+
+    // Mapping from token id to position in the allUsedTokens array
+    mapping(uint256 => uint256) private _allUsedTokensIndex;
+
+    // Mapping from address to its number of used tokens
+    mapping(address => uint256) private _usedBalances;
+
+}
