1e494f23e50fb05d1e5d28964dfebdf3042cf213
Emile Calixte <calixte.emile@gmail.com>
2022-12-02 16:02:50 +0100

    Add state variables, constants and balances in Subscription

--- contracts/Subscription.sol
+++ contracts/Subscription.sol
@@ -11,14 +11,53 @@ import "./Marketplace.sol";
 contract Subscription is Initializable, ERC4907EnumerableUpgradeable, ERC721EnumerableUpgradeable, Marketplace {
     using CountersUpgradeable for CountersUpgradeable.Counter;
 
+    uint256 private constant RENTING_REQUEST_TIMEOUT = 120; // Number of seconds a request is valid
+
+    uint256 private constant CONTENT_PROVIDER_COMMISSION_PERCENTAGE = 15; // 15%
+
+    uint256 private constant MARKETPLACE_PROVIDER_COMMISSION_PERCENTAGE = 15; // 15%
+
+    struct RentingRequest {
+        uint256 tokenId;
+        uint256 price; // Renting price in wei
+        uint256 expires; // Expiration timestamp of the request
+    }
+
     CountersUpgradeable.Counter private _tokenIds;
 
+    CountersUpgradeable.Counter private _rentingRequestIds;
+
+    // Mapping from tokenId to subscription expiration timestamp
     mapping(uint256 => uint256) private _expirations;
 
-    function initialize(string calldata name_, string calldata symbol_) public initializer {
+    // Mapping from renting request ID to the renting request data
+    mapping(uint256 => RentingRequest) private _rentingRequests;
+
+    // The address of the entity providing content or service
+    address private _contentProvider;
+
+    // The address of the subscription renting system provider
+    address private _marketplaceProvider;
+
+    // Mapping from address to balance in wei
+    mapping(address => uint256) public balances;
+
+    event RentingRequestCreated(uint256 requestId, uint256 price, uint256 expires);
+
+    function initialize(
+        string calldata name_,
+        string calldata symbol_,
+        address contentProvider_,
+        address marketplaceProvider_
+    ) public initializer {
         ERC4907EnumerableUpgradeable.__ERC4907Enumerable_init(name_, symbol_);
         Marketplace.__Marketplace_init();
+
+        _contentProvider = contentProvider_;
+        _marketplaceProvider = marketplaceProvider_;
+
         _tokenIds.increment();
+        _rentingRequestIds.increment();
     }
 
     function mint() public {
@@ -32,6 +71,18 @@ contract Subscription is Initializable, ERC4907EnumerableUpgradeable, ERC721Enum
         require(block.timestamp >= userExpires(tokenId), "Already used");
 
         super.setUser(tokenId, user, expires);
+    }
+
+    function withdraw() public {
+        uint256 value = balances[msg.sender];
+
+        require(value > 0, "Nothing to withdraw");
+
+        balances[msg.sender] = 0;
+
+        (bool sent,) = msg.sender.call{value: value}("");
+
+        require(sent, "Failed to send Ether");
     }
 
     /*
