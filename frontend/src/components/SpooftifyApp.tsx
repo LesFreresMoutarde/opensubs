d6b0c016c9a62617e972a382ad5f8743e67e1926
alexisljn <alexis.lejeune@gmx.fr>
2022-12-05 16:35:32 +0100

    Initiate Provider in Spooftify

--- frontend/src/components/SpooftifyApp.tsx
+++ frontend/src/components/SpooftifyApp.tsx
@@ -1,4 +1,20 @@
+import {useEffect, useState} from "react";
+import {providers} from "ethers";
+
 function SpooftifyApp() {
+
+    const [provider, setProvider] = useState<providers.Web3Provider | null | undefined>(undefined);
+
+    useEffect(() => {
+        if (window.ethereum) {
+            setProvider(new providers.Web3Provider(window.ethereum));
+        } else {
+            //TODO Display div "you need metamask"
+            setProvider(null);
+        }
+
+    }, []);
+
     return (
         <p>Spooftify</p>
     )
