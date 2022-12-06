01f5e63f93900bf741314080a900d746a2017028
Emile Calixte <calixte.emile@gmail.com>
2022-12-06 10:48:20 +0100

    Style portal

--- frontend/src/components/Portal.tsx
+++ frontend/src/components/Portal.tsx
@@ -1,18 +1,40 @@
 import { Link } from "react-router-dom"
+import "../css/portal.css"
 
 function Portal() {
     return (
-        <nav>
-            <ul>
-                <Link to="/opensubs">Opensubs</Link>
-            </ul>
-            <ul>
-                <Link to="/fakeflix">Fakeflix</Link>
-            </ul>
-            <ul>
-                <Link to="/spooftify">Spooftify</Link>
-            </ul>
-        </nav>
+        <div id="portal">
+
+
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/opensubs">
+                            <div className="logo">
+                                <img src="/portal/img/opensubs-logo.png" alt="Logo Opensubs"/>
+                            </div>
+                            <div className="label">Opensubs</div>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/fakeflix">
+                            <div className="logo">
+                                <img src="/portal/img/fakeflix-logo.png" alt="Logo Fakeflix"/>
+                            </div>
+                            <div className="label">Fakeflix</div>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/spooftify">
+                            <div className="logo">
+                                <img src="/portal/img/spooftify-logo.png" alt="Logo Fakeflix"/>
+                            </div>
+                            <div className="label">Spooftify</div>
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
