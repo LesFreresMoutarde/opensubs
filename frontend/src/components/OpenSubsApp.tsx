88e18dabbb8e6b3f11ddd02129d892002a4095b9
Emile Calixte <calixte.emile@gmail.com>
2022-12-06 14:28:21 +0100

    Initialize contracts in OpenSubs

--- frontend/src/components/OpenSubsApp.tsx
+++ frontend/src/components/OpenSubsApp.tsx
@@ -1,13 +1,39 @@
 import {useEffect, useState} from "react";
-import {providers} from "ethers";
+import {Contract, providers} from "ethers";
 import ConnectButton from "./common/ConnectButton";
 import {autoLogin} from "../utils/ProviderUtils";
+import {getSubscriptionContract} from "../utils/SubscriptionUtil";
+
+type ContractDescription = {
+    /**
+     * The address of the contract
+     */
+    address: string;
+
+    /**
+     * The contract instance
+     */
+    contract: Contract;
+}
+
+type ServiceName = "fakeflix" | "spooftify";
+
+type ContractsList = {
+    [key in ServiceName]: ContractDescription;
+};
+
+const contractAddresses: {[key in ServiceName]: string} = {
+    fakeflix: String(process.env.REACT_APP_FAKEFLIX_CONTRACT_ADDRESS),
+    spooftify: String(process.env.REACT_APP_SPOOFTIFY_CONTRACT_ADDRESS),
+};
 
 function OpenSubsApp() {
 
     const [provider, setProvider] = useState<providers.Web3Provider | null | undefined>(undefined);
 
     const [address, setAddress] = useState('');
+
+    const [contracts, setContracts] = useState<ContractsList | null>(null);
 
     useEffect(() => {
         (async () => {
@@ -26,6 +52,23 @@ function OpenSubsApp() {
             }
         })();
     }, []);
+
+    useEffect(() => {
+        if (!provider) {
+            return;
+        }
+
+        const contractDescriptions: Partial<ContractsList> = {};
+
+        for (const [serviceName, address] of Object.entries(contractAddresses)) {
+            contractDescriptions[serviceName as ServiceName] = {
+                address,
+                contract: getSubscriptionContract(provider, address),
+            };
+        }
+
+        setContracts(contractDescriptions as ContractsList);
+    }, [provider]);
 
     if (provider === undefined) {
         return (
