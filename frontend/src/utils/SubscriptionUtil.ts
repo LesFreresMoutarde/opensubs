cf70f9cb685167d4fdde76e30e4bc8762bdf4b6f
alexisljn <alexis.lejeune@gmx.fr>
2022-12-06 17:24:38 +0100

    Create getTokensOwnedByUser() and isChainIdSupported()

--- frontend/src/utils/SubscriptionUtil.ts
+++ frontend/src/utils/SubscriptionUtil.ts
@@ -1,8 +1,18 @@
-import {Contract, ethers, providers} from "ethers";
+import {BigNumber, Contract, ethers, providers} from "ethers";
 import SUBSCRIPTION_JSON from "../artifacts/contracts/Subscription.sol/Subscription.json";
 
 function getSubscriptionContract(provider: providers.Web3Provider, address: string): Contract {
     return new ethers.Contract(address, SUBSCRIPTION_JSON.abi, provider.getSigner());
 }
 
-export {getSubscriptionContract}
+async function getTokensOwnedByUser(contract: Contract, address: string): Promise<BigNumber> {
+    return await contract.balanceOf(address);
+}
+
+function isChainIdSupported(chainId: number): boolean {
+    const supportedNetworks = [5, 31337];
+
+    return supportedNetworks.includes(chainId);
+}
+
+export {getSubscriptionContract, getTokensOwnedByUser, isChainIdSupported}
