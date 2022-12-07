27052a766e28721652063522d3b5d3db22fb66ea
Emile Calixte <calixte.emile@gmail.com>
2022-12-07 11:57:28 +0100

    Add music player

--- frontend/src/components/spooftify/SpooftifyContent.tsx
+++ frontend/src/components/spooftify/SpooftifyContent.tsx
@@ -1,9 +1,11 @@
 import ContentList from "./ContentList";
+import AudioPlayer from "./AudioPlayer";
 
 function SpooftifyContent() {
     return (
         <div>
             <ContentList/>
+            <AudioPlayer/>
         </div>
     );
 }
