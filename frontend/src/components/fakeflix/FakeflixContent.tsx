68c88bed4a3b5af3c351951a10c8ad80e15dd733
Emile Calixte <calixte.emile@gmail.com>
2022-12-08 15:18:33 +0100

    Finish Fakeflix content page

--- frontend/src/components/fakeflix/FakeflixContent.tsx
+++ frontend/src/components/fakeflix/FakeflixContent.tsx
@@ -5,27 +5,41 @@ function FakeflixContent() {
     const {content, selectedItem, selectItem} = useContext(fakeflixAppContext);
 
     return (
-        <div className="movies-list">
-            {content.map((item, index) => {
-                return (
-                    <div key={index} className="movie">
-                        <div className="movie-cover">
-                            <img src={item.coverUrl} alt="Cover"/>
-                        </div>
-                        <div className="movie-data">
-                            <div className="title">
-                                {item.title}
-                            </div>
-                            <div className="director">
-                                {item.director}
-                            </div>
-                            <div className="actors">
-                                {item.actors.join(', ')}
-                            </div>
-                        </div>
+        <div>
+            {selectedItem &&
+            <div className="video-player-container">
+                <div className="inner">
+                    <video src={selectedItem[1].movieUrl} controls={true} autoPlay={true} />
+                    <div className="close-button-container">
+                        <button onClick={() => selectItem(null)}>
+                            Close
+                        </button>
                     </div>
-                );
-            })}
+                </div>
+            </div>
+            }
+            <div className="movies-list">
+                {content.map((item, index) => {
+                    return (
+                        <div key={index} className="movie">
+                            <div className="movie-cover">
+                                <img src={item.coverUrl} alt="Cover" onClick={() => selectItem(index)}/>
+                            </div>
+                            <div className="movie-data">
+                                <div className="title">
+                                    {item.title}
+                                </div>
+                                <div className="director">
+                                    {item.director}
+                                </div>
+                                <div className="actors">
+                                    {item.actors.join(', ')}
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
