09f7f2ed46693c64dcf9d2bbad767848f2334e3b
alexisljn <alexis.lejeune@gmx.fr>
2022-12-09 14:17:29 +0100

    create useEffect to handle Transfer event in FakeflixMint

--- frontend/src/components/fakeflix/FakeflixMint.tsx
+++ frontend/src/components/fakeflix/FakeflixMint.tsx
@@ -1,4 +1,35 @@
+import {useCallback, useContext, useEffect, useState} from "react";
+import {mintToken} from "../../utils/SubscriptionUtil";
+import {fakeflixAppContext} from "../FakeflixApp";
+import LoadingModal from "../common/LoadingModal";
+import {fireToast} from "../../utils/Util";
+import {ethers} from "ethers";
+
 function FakeflixMint() {
+
+    const {subscription, provider, address} = useContext(fakeflixAppContext);
+
+    const [showModal, setShowModal] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (!subscription) {
+            return;
+        }
+
+        subscription.on('Transfer', (from, to, tokenId) => {
+            console.log("from, to, tokenId", from, to ,tokenId);
+            console.log(address);
+
+            if (to === ethers.utils.getAddress(address)) {
+                setShowModal(false);
+
+                fireToast('success', 'You have successfully minted a subscription NFT');
+
+                console.log("push metadata")
+            }
+        });
+    }, [subscription]);
+
     return (
         <p>Fakeflix mint</p>
     );
