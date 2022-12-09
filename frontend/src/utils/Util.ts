dce5875365deace89aadd00f5c04a0bcb3ac143f
alexisljn <alexis.lejeune@gmx.fr>
2022-12-09 14:12:46 +0100

    create fireToast

--- frontend/src/utils/Util.ts
+++ frontend/src/utils/Util.ts
@@ -1,3 +1,5 @@
+import Swal from 'sweetalert2';
+
 function shortenAddress(address: string, firstCharactersCount: number = 3, lastCharactersCount: number = 3): string {
     if (!address.startsWith("0x")) {
         return address;
@@ -9,4 +11,23 @@ function shortenAddress(address: string, firstCharactersCount: number = 3, lastC
     return firstCharacters + "…" + lastCharacters;
 }
 
-export {shortenAddress}
+type toastType = "success" | "error" | "warning"
+
+function fireToast(type: toastType, text: string) {
+    const bgColor: {[key: string]: string} = {success: '#28a745', error: '#dc3545', warning: '#ffc107'};
+
+    const textColor = {success: 'white', error: 'white', warning: '#343a40'};
+
+    Swal.fire({
+        title: text,
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        background: bgColor[type],
+        color: textColor[type]
+    });
+}
+
+export {shortenAddress, fireToast}
