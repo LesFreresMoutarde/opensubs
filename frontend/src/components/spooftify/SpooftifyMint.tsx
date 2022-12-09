43134489cc55315f34ace1a5d2339f2cf7c4dbfa
alexisljn <alexis.lejeune@gmx.fr>
2022-12-09 15:25:01 +0100

    mint in Spooftify

--- frontend/src/components/spooftify/SpooftifyMint.tsx
+++ frontend/src/components/spooftify/SpooftifyMint.tsx
@@ -1,6 +1,52 @@
+import {useCallback, useContext, useEffect, useState} from "react";
+import {ethers} from "ethers";
+import {fireToast, pushMetadata} from "../../utils/Util";
+import {mintToken} from "../../utils/SubscriptionUtil";
+import LoadingModal from "../common/LoadingModal";
+import {spooftifyAppContext} from "../SpooftifyApp";
+
 function SpooftifyMint() {
+
+    const {subscription, provider, address} = useContext(spooftifyAppContext);
+
+    const [showModal, setShowModal] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (!subscription) {
+            return;
+        }
+
+        subscription.on('Transfer', (from, to, tokenId) => {
+            if (to === ethers.utils.getAddress(address)) {
+                setShowModal(false);
+
+                fireToast('success', 'You have successfully minted a subscription NFT');
+
+                pushMetadata(tokenId, 'spooftify');
+            }
+        });
+    }, [subscription]);
+
+    const mint = useCallback(async () => {
+        if (!subscription) {
+            return;
+        }
+
+        await mintToken(subscription, provider!);
+
+        setShowModal(true);
+    }, [subscription, provider]);
+
+    const closeModal = useCallback(() => {
+        setShowModal(false);
+    }, [])
+
     return (
-        <p>Spooftify mint</p>
+        <>
+            {showModal && <LoadingModal showModal={showModal} closeModal={closeModal}/>}
+            <p>Spooftify mint</p>
+            <button onClick={mint}>Mint</button>
+        </>
     );
 }
 
