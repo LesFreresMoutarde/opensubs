5ebad5f7d2aea6aed346642287a573d349f70d5c
Emile Calixte <calixte.emile@gmail.com>
2022-12-11 12:16:01 +0100

    Add title to error page

--- frontend/src/components/ErrorPage.tsx
+++ frontend/src/components/ErrorPage.tsx
@@ -1,4 +1,16 @@
+import {useEffect} from "react";
+
 function ErrorPage() {
+    useEffect(() => {
+        const initialTitle = document.title;
+
+        document.title = "Error";
+
+        return (() => {
+            document.title = initialTitle;
+        })
+    }, []);
+
     return (
         <p>Error !</p>
     )
