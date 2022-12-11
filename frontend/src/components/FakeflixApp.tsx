817c774db855ffc717abdba312340cce78ab14b8
Emile Calixte <calixte.emile@gmail.com>
2022-12-11 12:17:04 +0100

    Add favicon to fakeflix

--- frontend/src/components/FakeflixApp.tsx
+++ frontend/src/components/FakeflixApp.tsx
@@ -95,6 +95,22 @@ function FakeflixApp() {
     }, []);
 
     useEffect(() => {
+        const initialLink = document.querySelector("head link[rel=icon]") as HTMLLinkElement;
+
+        const newLink: HTMLLinkElement = document.createElement("link");
+        newLink.rel = "icon";
+        newLink.href = "/ico/fakeflix.ico";
+
+        document.head.removeChild(initialLink);
+        document.head.appendChild(newLink);
+
+        return (() => {
+            document.head.removeChild(newLink);
+            document.head.appendChild(initialLink);
+        });
+    }, []);
+
+    useEffect(() => {
         const initialTitle = document.title;
 
         document.title = "Fakeflix";
