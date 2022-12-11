10a5785304de89f07410459c698fa3b203ee1801
alexisljn <alexis.lejeune@gmx.fr>
2022-12-11 11:43:15 +0100

    style for Error Page

--- frontend/src/components/ErrorPage.tsx
+++ frontend/src/components/ErrorPage.tsx
@@ -1,6 +1,8 @@
 function ErrorPage() {
     return (
-        <p>Error !</p>
+        <div className="container-fluid d-flex align-items-center justify-content-center vh-100">
+            <h1 className="text-white">404 - Not Found</h1>
+        </div>
     )
 }
 
